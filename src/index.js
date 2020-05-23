9010652fd2ba7a8f8f8bfb18d8108b7d82e9e58b
Franz <franzcmarcelo@gmail.com>
2020-05-23 00:41:59 -0500

    Done Part 3.PrimerComponente

--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import Badge from './components/Badge';
 
 const container = document.getElementById('app');
 
+// Badge (sin los <>, representa un componente)
+// Pero a ReactDOM.render hay que darle un elemento: <Badge />
+
 ReactDOM.render(<Badge />, container);
