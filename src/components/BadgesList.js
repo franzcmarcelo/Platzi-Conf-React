74dfc20c2ce4521eca2a752d92f7fd8bde7017a4
Franz <franzcmarcelo@gmail.com>
2020-05-25 05:50:12 -0500

    Done Part 25.SearchFilter

--- src/components/BadgesList.js
+++ src/components/BadgesList.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './styles/BadgesList.css';
-import Gravatar from './Gravatar';
+import "./styles/BadgesList.css";
+import Gravatar from "./Gravatar";
 
 class BadgesListItem extends React.Component {
   render() {
@@ -27,11 +27,21 @@ class BadgesListItem extends React.Component {
 }
 
 function useSearchBadges(badges) {
-  const [query, setQuery] = React.useState('');
+  // Representado el input del Search
+  const [query, setQuery] = React.useState("");
+
+  // De aqui obtendremos todos badges que coincidan con la busqueda
   const [filteredBadges, setFilteredBadges] = React.useState(badges);
 
+  // FIXME:
+  // Hook useMemo: Permite optimizar la busqueda (como una especie de cache)
+  // Recibe dos argumentos:
+  // 1. la funcion con la logica (trabajando con badges y query)
+  // 2. una lista con los argumentos que siempre que sean iguales devolvera
+  // la respuesta ya guardada, sino, calcula la respuesta por primera vez
   React.useMemo(() => {
-    const result = badges.filter(badge => {
+    // Devolvemos un array con todos los badges que cumplan el filtro
+    const result = badges.filter((badge) => {
       return `${badge.firstName} ${badge.lastName}`
         .toLowerCase()
         .includes(query.toLowerCase());
@@ -40,6 +50,10 @@ function useSearchBadges(badges) {
     setFilteredBadges(result);
   }, [badges, query]);
 
+  // Retornamos:
+  // query: aqui tendremos el valor del input
+  // setQuery: iremos modificando el valor de query, mediante onChange
+  // filteredBadges: los badges que coincidieron con la busqueda
   return { query, setQuery, filteredBadges };
 }
 
@@ -48,8 +62,10 @@ function BadgesList(props) {
 
   const { query, setQuery, filteredBadges } = useSearchBadges(badges);
 
+  // Si ningun badge conincide con la busqueda
   if (filteredBadges.length === 0) {
     return (
+      // Mantenemos el Search
       <div>
         <div className="form-group">
           <label>Filter Badges</label>
@@ -57,12 +73,13 @@ function BadgesList(props) {
             type="text"
             className="form-control"
             value={query}
-            onChange={e => {
+            onChange={(e) => {
               setQuery(e.target.value);
+              console.log(e.target.value);
             }}
           />
         </div>
-
+        {/* Opcion para regresar */}
         <h3>No badges were found</h3>
         <Link className="btn btn-primary" to="/badges/new">
           Create new badge
@@ -78,15 +95,21 @@ function BadgesList(props) {
         <input
           type="text"
           className="form-control"
+          // Al llenar el input, mediante onChange
+          // actualizamos el estado de query
           value={query}
-          onChange={e => {
+          onChange={(e) => {
             setQuery(e.target.value);
           }}
         />
       </div>
 
       <ul className="list-unstyled">
-        {filteredBadges.map(badge => {
+        {/* Mapeamos los badge que coinciden con la
+        busqueda y los renderizamos */}
+        {/* Como inicialmente el valor de query es "" esta
+        include en cualquier, entonces se renderizan todos*/}
+        {filteredBadges.map((badge) => {
           return (
             <li key={badge.id}>
               <Link
